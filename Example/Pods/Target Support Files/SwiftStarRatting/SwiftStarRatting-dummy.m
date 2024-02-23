a88324ba8bb68be4d25ccf654653292d29400316
@MI-490 <narendra.singh@mindinventory.com>
2024-02-23 16:16:24 +0530

    Initial commit with cocoapod lib

---
+++ Example/Pods/Target Support Files/SwiftStarRatting/SwiftStarRatting-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SwiftStarRatting : NSObject
+@end
+@implementation PodsDummy_SwiftStarRatting
+@end
---
+++ Example/SwiftStarRatting/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwiftStarRatting
+//
+//  Created by 87113814 on 02/23/2024.
+//  Copyright (c) 2024 87113814. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
